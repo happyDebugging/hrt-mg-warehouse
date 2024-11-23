d34df8aacfa5dbe4c69a83884f9bd5977760f4fa
happyDebugging <87609041+happyDebugging@users.noreply.github.com>
2024-11-23 02:58:06 +0200

    Implemented actions history preview.

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -3,11 +3,8 @@ import { DbFunctionService } from '../shared/services/db-functions.service';
 import { map, Subscription } from 'rxjs';
 import { HistoryLines } from '../shared/models/history-lines.model';
 import { initializeApp } from 'firebase/app';
-import { getDatabase, query, startAt } from 'firebase/database';
-import { getStorage, ref } from 'firebase/storage';
-import { orderBy } from 'firebase/firestore';
-import { queryRef } from 'firebase/data-connect';
-import firebase from 'firebase/compat/app';
+import { child, get, getDatabase, ref, onValue, query, startAt } from 'firebase/database';
+import { collection, getFirestore, orderBy } from 'firebase/firestore';
 
 @Component({
   selector: 'app-history',
@@ -16,14 +13,28 @@ import firebase from 'firebase/compat/app';
 })
 export class HistoryComponent {
 
-  historyLinesList = [{
-    Id: '',
-    Date: '',
-    ActionType: '',
-    MaterialName: '',
-    SerialNumber: '',
-    Responsible: ''
-  }];
+  pageCount = 1;
+  historyRows = 10;
+  leftPageNum = 1;
+  midPageNum = 2;
+  rightPageNum = 3;
+
+  // historyLinesList: Array<{
+  //   Id: '',
+  //   Date: '',
+  //   ActionType: '',
+  //   MaterialName: '',
+  //   SerialNumber: '',
+  //   Responsible: ''
+  // }> = [{
+  //   Id: '',
+  //   Date: '',
+  //   ActionType: '',
+  //   MaterialName: '',
+  //   SerialNumber: '',
+  //   Responsible: ''
+  // }];
+  historyLinesList: Array<HistoryLines[]> = [];
 
   getHistoryLines: Subscription = new Subscription;
 
@@ -43,10 +54,6 @@ export class HistoryComponent {
 
   // Initialize Realtime Database and get a reference to the service
   database = getDatabase(this.firebaseApp);
-  // Initialize Cloud Storage and get a reference to the service
-  storage = getStorage(this.firebaseApp);
-  // Create a storage reference from our storage service
-  storageRef = ref(this.storage, 'some-child');
 
   constructor(private dbFunctionService: DbFunctionService) { }
 
@@ -57,60 +64,144 @@ export class HistoryComponent {
   GetHistoryLines() {
     this.historyLinesList = [];
 
-    this.getHistoryLines = this.dbFunctionService.geHistoryLinesFromDb()
-      .pipe(map((response: any) => {
-        let markerArray: HistoryLines[] = [];
+    // this.getHistoryLines = this.dbFunctionService.geHistoryLinesFromDb()
+    //   .pipe(map((response: any) => {
+    //     let markerArray: HistoryLines[] = [];
 
-        for (const key in response) {
-          if (response.hasOwnProperty(key)) {
+    //     for (const key in response) {
+    //       if (response.hasOwnProperty(key)) {
 
-            markerArray.push({ ...response[key], Id: key })
+    //         markerArray.push({ ...response[key], Id: key })
 
+    //       }
+    //     }
+
+    //     return markerArray.reverse();
+
+    //   }))
+    //   .subscribe(
+    //     (res: any) => {
+    //       if ((res != null) || (res != undefined)) {
+    //         console.log(res)
+    //         const responseData = new Array<HistoryLines>(...res);
+
+    //         for (const data of responseData) {
+
+    //           const resObj = new HistoryLines();
+
+    //           resObj.Id = data.Id;
+    //           resObj.Date = data.Date;
+    //           resObj.ActionType = data.ActionType;
+    //           resObj.MaterialName = data.MaterialName;
+    //           resObj.SerialNumber = data.SerialNumber;
+    //           resObj.Responsible = data.Responsible;
+
+    //           this.historyLinesList.push(resObj);
+
+    //         }
+
+    //         //this.historyLinesList.reverse();
+
+    //       }
+    //     },
+    //     err => {
+    //       //console.log(err);
+    //     }
+    //   );
+
+
+    const startCountRef = ref(this.database, '/history');
+    let historyLines = new Array<HistoryLines>();
+    onValue(startCountRef, (snapshot) => {
+      const res = snapshot.val();
+
+      let markerArray: HistoryLines[] = [];
+
+      for (const key in res) {
+        if (res.hasOwnProperty(key)) {
+
+          markerArray.push({ ...res[key], Id: key })
+
+        }
+      }
+      markerArray.reverse();
+
+      console.log(markerArray)
+      let counter = 0;
+      let index = 0;
+      console.log(Math.ceil(markerArray.length / this.historyRows))
+
+      for (var y = 0; y <= Math.ceil(markerArray.length / this.historyRows); y++) {
+        historyLines = [];
+
+        for (var i = y + counter; i <= y + counter + (this.historyRows - 1); i++) {
+          let resObj = new HistoryLines();
+
+          if (markerArray[i] != undefined) {
+            resObj.Id = markerArray[i].Id;
+            resObj.Date = markerArray[i].Date;
+            resObj.ActionType = markerArray[i].ActionType;
+            resObj.MaterialName = markerArray[i].MaterialName;
+            resObj.SerialNumber = markerArray[i].SerialNumber;
+            resObj.Responsible = markerArray[i].Responsible;
+
+            historyLines.push(resObj);
           }
         }
 
-        return markerArray.reverse();
+        this.historyLinesList[index] = historyLines;
 
-      }))
-      .subscribe(
-        (res: any) => {
-          if ((res != null) || (res != undefined)) {
-            console.log(res)
-            const responseData = new Array<HistoryLines>(...res);
+        counter += (this.historyRows - 1);
+        index++;
+      }
 
-            for (const data of responseData) {
+      console.log(this.historyLinesList)
 
-              const resObj = new HistoryLines();
+    });
 
-              resObj.Id = data.Id;
-              resObj.Date = data.Date;
-              resObj.ActionType = data.ActionType;
-              resObj.MaterialName = data.MaterialName;
-              resObj.SerialNumber = data.SerialNumber;
-              resObj.Responsible = data.Responsible;
 
-              this.historyLinesList.push(resObj);
-
-            }
-
-            //this.historyLinesList.reverse();
-
-          }
-        },
-        err => {
-          //console.log(err);
-        }
-      );
   }
 
   nextPage() {
+    if (this.pageCount < this.historyLinesList.length - 1) {
+      this.pageCount++;
+      console.log(this.pageCount)
+      console.log(this.historyLinesList[this.pageCount])
 
-    const db = firebase.database();
-    const postsRef = db.ref('posts');
-    postsRef.orderByChild('created_at').limitToLast(10).once('value', (snapshot) => {
-      // Process the snapshot data here
-      console.log('snapshot '+snapshot)
-    });
+      if (this.pageCount > 3) {
+        this.UpdatePageNumbers();
+      }
+
+    }
+  }
+
+  previousPage() {
+
+    if (this.pageCount > 1) {
+      this.pageCount--;
+      console.log(this.pageCount)
+      console.log(this.historyLinesList[this.pageCount])
+    }
+  }
+
+  setPageNumber(num: number) {
+
+    this.pageCount = num;
+    console.log(this.pageCount)
+    console.log(this.historyLinesList[this.pageCount])
+
+  }
+
+  UpdatePageNumbers() {
+    if (this.pageCount > this.midPageNum) {
+      this.leftPageNum++;
+      this.midPageNum++;
+      this.rightPageNum++;
+    } else if (this.pageCount < this.midPageNum) {
+      this.leftPageNum--;
+      this.midPageNum--;
+      this.rightPageNum--;
+    }
 
   }
 
