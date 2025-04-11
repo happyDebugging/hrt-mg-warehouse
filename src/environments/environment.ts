4f74cf10c16fd7469c4c8afa20c2a839da39cf0c
happyDebugging <87609041+happyDebugging@users.noreply.github.com>
2025-04-11 19:35:22 +0200

    Added Service Role Key in environments.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,5 +10,7 @@ export const environment = {
 
     supabaseUrl: 'https://obuungnbgbhihzfprqsj.supabase.co',
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9idXVuZ25iZ2JoaWh6ZnBycXNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwNjIxMTIsImV4cCI6MjA1NzYzODExMn0.Y5-3mcMRRkHK0-1neFYw0CoY00ST-xGWAa7zWOz6k40',
+    supabaseServiceRoleKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9idXVuZ25iZ2JoaWh6ZnBycXNqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MjA2MjExMiwiZXhwIjoyMDU3NjM4MTEyfQ.v-AbToSIfomYEzRldxrf10To2dSkb-_vQ8_DG5dtifU',
+
 };
 
